a19cbdabc55510862a8414fe14e0b0e6c9d3e577
Shubbu-07 <95517382+Shubbu-07@users.noreply.github.com>
2024-01-12 23:41:27 +0530

    Addes Navbar & About components.

---
+++ src/components/About/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './About.css'
+
+export default function About() {
+  return (
+    <div className='container'>
+      <div className='home-content'>
+                <h1>Hi! I'm Shubham Mergu</h1>
+                <h4 className='my-3'>a self taught Web Developer</h4>
+                <p className='my-3'>Creative Web Developer Offering 4+ Years Of Experience Providing <br /> High-Impact Web Solutions For Many Different Organizations.</p>
+            </div>
+    </div>
+  )
+}
